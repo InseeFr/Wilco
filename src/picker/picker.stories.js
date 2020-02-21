ee1ec7105ef763c5ab4ffc15592af11bd4765e2d
Nicolas Laval <nicolas.laval@insee.fr>
2020-02-21 11:02:56 +0100

    Fix picker story

--- src/picker/picker.stories.js
+++ src/picker/picker.stories.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { MemoryRouter } from 'react-router-dom';
 import Picker from '.';
 
 import { withKnobs, text } from '@storybook/addon-knobs';
 
 const stories = storiesOf('Picker', module);
 stories.addDecorator(withKnobs);
+
+const routerDecorator = storyFn => <MemoryRouter>{storyFn()}</MemoryRouter>;
+stories.addDecorator(routerDecorator);
 
 const styleDecorator = storyFn => (
 	<div className="col-md-10 col-md-offset-1">{storyFn()}</div>
