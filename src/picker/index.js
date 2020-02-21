0e0410af2af2245ce45dce9af246377b4a06dd5e
Nicolas Laval <nicolas.laval@insee.fr>
2020-02-21 11:51:03 +0100

    Delete unused import

--- src/picker/index.js
+++ src/picker/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import AddLogo from '../logo/logo-add';
 import DelLogo from '../logo/logo-del';
