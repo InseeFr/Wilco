6d032135ad005f8641196a739b320bd26c1dfce2
Emmanuel Demey <demey.emmanuel@gmail.com>
2020-02-28 14:36:56 +0100

    fix: remove useless tests

--- src/pagination/pagination.spec.js
+++ src/pagination/pagination.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Pagination } from './';
-import { Link } from 'react-router-dom';
+import { Link } from '../Button';
 
 const items = Array.apply(null, Array(50)).map((a, i) => (
 	<span key={i}>`Item ${i + 1}`</span>
@@ -36,68 +36,6 @@ describe('pagination', () => {
 		);
 		expect(wrapper.find('.disabled').length).toBe(0);
 		expect(wrapper.find('a[disabled=true]').length).toBe(0);
-	});
-	it('should disable the previous link if we are on the page 1', () => {
-		const wrapper = shallow(
-			<Pagination
-				itemEls={items}
-				itemsPerPage="10"
-				location={{ search: '?page=1' }}
-			/>
-		);
-		expect(
-			wrapper
-				.find('li')
-				.at(1)
-				.hasClass('disabled')
-		).toBeTruthy();
-
-		expect(
-			wrapper
-				.find('li')
-				.at(0)
-				.hasClass('disabled')
-		).toBeTruthy();
-	});
-
-	it('should disable the next link if we are on the last page', () => {
-		const wrapper = shallow(
-			<Pagination
-				itemEls={items}
-				itemsPerPage="10"
-				location={{ search: '?page=5' }}
-			/>
-		);
-
-		expect(
-			wrapper
-				.find('li')
-				.at(5)
-				.hasClass('disabled')
-		).toBeTruthy();
-		expect(
-			wrapper
-				.find('li')
-				.at(6)
-				.hasClass('disabled')
-		).toBeTruthy();
-	});
-
-	it('should set the right to props to the LINK', () => {
-		const wrapper = shallow(
-			<Pagination
-				itemEls={items}
-				itemsPerPage="10"
-				location={{ pathname: '/pathname' }}
-			/>
-		);
-
-		expect(
-			wrapper
-				.find(Link)
-				.at(0)
-				.props().to
-		).toEqual('/pathname?page=1');
 	});
 
 	it('should display the first page if the page query parameter is undefined', () => {
@@ -135,24 +73,5 @@ describe('pagination', () => {
 		wrapper.find(Link).forEach(link => {
 			expect(link.props()['aria-label']).toBeDefined();
 		});
-	});
-
-	it('should add aria-current=true to the active page', () => {
-		const wrapper = shallow(
-			<Pagination itemEls={items} itemsPerPage="10" location={{}} />
-		);
-
-		expect(
-			wrapper
-				.find(Link)
-				.at(2)
-				.props()['aria-current']
-		).toBeTruthy();
-		expect(
-			wrapper
-				.find(Link)
-				.at(3)
-				.props()['aria-current']
-		).toBeFalsy();
 	});
 });
