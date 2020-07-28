3dfcfead618104cfa32ae36faaf66414f081cc85
Emmanuel Demey <demey.emmanuel@gmail.com>
2020-07-28 14:20:55 +0200

    feat: add unit test for picker-item component

--- src/picker-item/item.spec.js
+++ src/picker-item/item.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import PickerItem from '.';
-
+import { MemoryRouter } from 'react-router-dom';
 describe('picker-page', () => {
 	it('renders without crashing', () => {
 		render(
@@ -11,5 +11,30 @@ describe('picker-page', () => {
 				handleClick={() => console.log('action')}
 			/>
 		);
+	});
+	it('should have logo if the handleClick prop is defined', () => {
+		const { container } = render(
+			<PickerItem
+				id="id"
+				label="label"
+				logo={<span>logo</span>}
+				handleClick={() => console.log('action')}
+			/>
+		);
+		expect(container.querySelector('li').innerHTML).toContain('logo');
+	});
+
+	it('should have links if the to prop is defined', () => {
+		const { container } = render(
+			<MemoryRouter>
+				<PickerItem id="id" label="label" to="to" />
+			</MemoryRouter>
+		);
+		expect(container.querySelector('a')).toBeDefined();
+	});
+
+	it('should return a basic li element', () => {
+		const { container } = render(<PickerItem id="id" label="label" />);
+		expect(container.querySelector('li').innerHTML).toBe('label');
 	});
 });
